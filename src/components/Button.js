6b48fb89629418af32760c4edde82f72b90c86ce
shourygupta28 <samarth.g2001@gmail.com>
2022-01-13 21:24:07 +0530

    commit

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+export const Button = ({color, text, onClick}) => {
+    return (
+        <button 
+            onClick={onClick}
+            style={{backgroundColor: color}} 
+            className='btn'
+        > {text} 
+        </button>
+    )
+}
